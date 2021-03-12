c8da229c8e02d986b1871e7aa92d0035cacee7c9
Kali Live user <seol@itu.dk>
2021-03-12 18:34:44 +0000

    Exercise5 initial

---
+++ day2/Exercise05/victim.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+  char name[64];
+  printf("%p\n", name);  // Print address of buffer.
+  puts("What's your name?");
+  gets(name);
+  printf("Hello, %s!\n", name);
+  return 0;
+}
