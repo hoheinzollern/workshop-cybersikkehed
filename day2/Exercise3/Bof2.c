67ce2b5d331e54ba775a99ec2f14546c21a8becf
Architect <jabb@itu.dk>
2021-03-12 11:17:19 +0100

    Added exercise3 binaries

---
+++ day2/Exercise3/Bof2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int c = 0;
+    char bof[24];
+    
+    printf("Give me some input: ");
+    gets(bof);
+
+    if (c == 0xcafeface) {
+        printf("Contgratulations!\n");
+        printf("c now has the value %x\n", c);
+        exit(0);
+    }
+    printf("Not quite the solution...\n");
+    exit(0);
+}
